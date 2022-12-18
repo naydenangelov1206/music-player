6d23cf2e03b0b80771f1d7d59732d4221e74cd96
naydenangelov1206 <naydenangelov1206@gmail.com>
2022-12-19 01:57:57 +0200

    everything is done just need to fix the js a little bit more

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function nextSong() {
 }
 
 function updateProgress(e) {
-  const { duration, currentTime } = e.scrElement;
+  const { duration, currentTime } = e.srcElement;
   const progressPercent = (currentTime / duration) * 100;
   progress.style.width = `${progressPercent}%`;
 }
